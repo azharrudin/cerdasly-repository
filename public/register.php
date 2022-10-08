37389d889e3dbe7af3a92898f6be6d6a5b351094
azharrudin <azharrudin0905@gmail.com>
2022-10-08 09:46:36 +0700

    repository update

--- public/register.php
+++ public/register.php
@@ -5,8 +5,9 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css" rel="stylesheet">
         <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
         <link href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet">    
-        <link href="css/styles.css" rel="stylesheet">
-        <link href="css/components.css" rel="stylesheet">
+        <link href="/styles/styles.css" rel="stylesheet">
+        <link href="/styles/components.css" rel="stylesheet">
+        <link href="/styles/ui.css" rel="stylesheet">
         <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
         <style>
             .ui_create_account_container {
