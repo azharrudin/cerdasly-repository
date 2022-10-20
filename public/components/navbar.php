f63c115d0471304b5aa5d676f7278072c1fd4106
azharrudin <azharrudin0905@gmail.com>
2022-10-20 16:57:35 +0700

    repository update

--- public/components/navbar.php
+++ public/components/navbar.php
@@ -8,7 +8,7 @@ function navigationBar($user, $space = true){
     ? "<img onclick=\"window.location = '/profile/'\" src='$profile_image' height=35 style='border-radius: 100%;max-width: 35px;object-fit: cover;' loading='lazy'>" 
     : "";
     $profile_image_exist      = strlen($profile_image) > 0 ? "" : "display: none;";
-    $login_button             = strlen($profile_image) > 0 ? "" : "<a style=\"float: right;margin-left: 6px\" class=\"btn btn-primary btn-sm ml-1\" href='/'>Masuk</a>";
+    $login_button             = strlen($profile_image) > 0 ? "" : "<a style=\"float: right;margin-left: 6px\" class=\"btn btn-primary btn-sm ml-1\" href='/login'>Masuk</a>";
     $unreadNotificationsTotal = count($core->getUnreadNotifications($user)); 
     $space = $space ? '<div style="margin-top: 46px;"></div>' : "";
     return <<<EOF
@@ -20,10 +20,10 @@ function navigationBar($user, $space = true){
                     loading="lazy"
                     id="logo"
                 />
-                <a style="float: right;$profile_image_exist" id="profile" class="ui_circular_image-x35">
+                $login_button
+                <a style="float: right;$profile_image_exist" href="/profile/" id="profile" class="ui_circular_image-x35">
                     $profile_image_button
                 </a>
-                $login_button
                 <div class="ui_navbar_right_side">
                     <a href="/notifications"><span class="bi bi-bell ui_navbar_notification_bell"></span></a>
                     <a class="btn btn-danger btn-sm" type="button"  href="/ask">
