048a86139fa08996fea1448041b5ff4c78eed951
azharrudin <azharrudin0905@gmail.com>
2022-10-20 17:15:21 +0700

    repository update

--- public/login.php
+++ public/login.php
@@ -24,73 +24,75 @@
         }
     }
 ?>
-<head>
-    <title>Cerdasly - Masuk</title>
-    <meta name="description" content="Ayo login Cerdasly untuk mengakses banyak soal dan berdiskusi tugas sekolah kamu">
-    <meta name="title" content="Cerdasly - Masuk">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="/styles/styles.css" rel="stylesheet">
-    <link href="/styles/components.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css" rel="stylesheet">
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet">    
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6098201334142651"
-     crossorigin="anonymous"></script>
-    <style>
-        a:hover {
-            text-decoration: none;
-        }
-    </style>
-</head>
-<body>
-<div style="padding-top: 20px;height: 100%;" class="shapedividers_com-4184">
-    <center><img
-        src="/cerdasly.png"
-        height="70"
-        alt="Cerdasly Logo"
-        loading="lazy"
-      /></center>
-    <div class="lay-container-sm container " style="padding-top: 0px;">
-        <form action="#" method="post" class="box-a m-5" style="background-color: white;">
-            <center class="mb-3"><h4><b>Masuk dengan akunmu <br> atau <a href="/register">daftar disini</a></b></h4></center>
-            <?= msgcreate($info_message); ?>
-            <div class="form-group">
-                <div class="input-group cps-input-group">
-                    <span class="input-group-addon"><span class="bi bi-person-circle"></span></span>
-                    <input class="form-control" id="email" placeholder="Email atau username akun anda" name="login_email" style="border-left: none;">
-                </div> 
-                 <small class="text-primary"><a href="/resetpass">Lupa username dan email akun saya</a></small>
-            </div>
-            <div class="form-group">
-                <div class="input-group cps-input-group">
-                    <span class="input-group-addon"><span class="bi bi-key"></span></span>
-                    <input type="password" class="form-control" style="border-right: none;" id="password" placeholder="Password akun anda" name="login_password">
-                    <span style="border-left: none; " class="input-group-addon" id="ui_view_password" onclick="viewPassword()" ><span class="bi bi-eye-slash" id="ui_eye_icon"></span></span>
+<html>
+    <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6098201334142651"crossorigin="anonymous"></script>
+        <title>Cerdasly - Masuk</title>
+        <meta name="description" content="Ayo login Cerdasly untuk mengakses banyak soal dan berdiskusi tugas sekolah kamu">
+        <meta name="title" content="Cerdasly - Masuk">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="/styles/styles.css" rel="stylesheet">
+        <link href="/styles/components.css" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css" rel="stylesheet">
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet">    
+    
+        <style>
+            a:hover {
+                text-decoration: none;
+            }
+        </style>
+    </head>
+    <body>
+    <div style="padding-top: 20px;height: 100%;" class="shapedividers_com-4184">
+        <center><img
+            src="/cerdasly.png"
+            height="70"
+            alt="Cerdasly Logo"
+            loading="lazy"
+        /></center>
+        <div class="lay-container-sm container " style="padding-top: 0px;">
+            <form action="#" method="post" class="box-a m-5" style="background-color: white;">
+                <center class="mb-3"><h4><b>Masuk dengan akunmu <br> atau <a href="/register">daftar disini</a></b></h4></center>
+                <?= msgcreate($info_message); ?>
+                <div class="form-group">
+                    <div class="input-group cps-input-group">
+                        <span class="input-group-addon"><span class="bi bi-person-circle"></span></span>
+                        <input class="form-control" id="email" placeholder="Email atau username akun anda" name="login_email" style="border-left: none;">
+                    </div> 
+                    <small class="text-primary"><a href="/resetpass">Lupa username dan email akun saya</a></small>
                 </div>
-                <small class="text-primary"><a href="/resetpass">Lupa password akun saya</a></small>
-            </div>
-            <button type="submit" class="btn btn-danger cps-btn" style="width: 100%;border-radius: 15px;">Masuk</button><br>
-            <center  style="margin-top: 11px;"><small>Belum punya akun? <a class="link-primary" href="/register"> Ayo daftar disini</a></small></center>
-        </form>
+                <div class="form-group">
+                    <div class="input-group cps-input-group">
+                        <span class="input-group-addon"><span class="bi bi-key"></span></span>
+                        <input type="password" class="form-control" style="border-right: none;" id="password" placeholder="Password akun anda" name="login_password">
+                        <span style="border-left: none; " class="input-group-addon" id="ui_view_password" onclick="viewPassword()" ><span class="bi bi-eye-slash" id="ui_eye_icon"></span></span>
+                    </div>
+                    <small class="text-primary"><a href="/resetpass">Lupa password akun saya</a></small>
+                </div>
+                <button type="submit" class="btn btn-danger cps-btn" style="width: 100%;border-radius: 15px;">Masuk</button><br>
+                <center  style="margin-top: 11px;"><small>Belum punya akun? <a class="link-primary" href="/register"> Ayo daftar disini</a></small></center>
+            </form>
+        </div>
+        <div class="flex lay-container-sm container " >
+            <center>
+                <a href="public/company.php" class="mb-5" style="float: left;border-bottom: 1px dotted #337AB7;">Ketentuan dan Persyaratan</a>
+                <a href="public/company.php" class="mb-5" style="float: right;border-bottom: 1px dotted #337AB7;">FAQ</a>
+            </center>
+        </div>
     </div>
-    <div class="flex lay-container-sm container " >
-        <center>
-            <a href="public/company.php" class="mb-5" style="float: left;border-bottom: 1px dotted #337AB7;">Ketentuan dan Persyaratan</a>
-            <a href="public/company.php" class="mb-5" style="float: right;border-bottom: 1px dotted #337AB7;">FAQ</a>
-        </center>
-    </div>
-</div>
 
-<script>
-    function viewPassword(){
-        if($("#password").attr("type") == "password"){
-            $("#ui_eye_icon").attr("class", "bi bi-eye")
-            $('#password').attr('type', 'text')
+    <script>
+        function viewPassword(){
+            if($("#password").attr("type") == "password"){
+                $("#ui_eye_icon").attr("class", "bi bi-eye")
+                $('#password').attr('type', 'text')
+            }
+            else {
+                $("#ui_eye_icon").attr("class", "bi bi-eye-slash")
+                $('#password').attr('type', 'password')
+            }
         }
-        else {
-            $("#ui_eye_icon").attr("class", "bi bi-eye-slash")
-            $('#password').attr('type', 'password')
-        }
-    }
-</script>
-</body>
+    </script>
+    </body>
+</html>
