06ff84b053e75590db74a3a0be6b88f89e8b2498
azharrudin <azharrudin0905@gmail.com>
2022-10-26 08:27:46 +0700

    repository update

--- php/email.php
+++ php/email.php
@@ -18,10 +18,10 @@ require_once(__DIR__ . "/../vendor/phpmailer/phpmailer/src/Exception.php");
 require_once(__DIR__ . "/../vendor/phpmailer/phpmailer/src/PHPMailer.php");
 require_once(__DIR__ . "/../vendor/phpmailer/phpmailer/src/SMTP.php");
 function sendVerification($to, $code, $message = ""){
-
+    // default message
     if(strlen($message) == 0){
-        $message     = "Hai, selamat datang di Cerdasly, untuk mengaktifkan akun yang sedang dibuat, anda dapat menggunakan kode verifikasi: <h4><tt>$code</tt></h4>.";
-        $message_alt = "Hai, selamat datang di Cerdasly, untuk mengaktifkan akun yang sedang dibuat, anda dapat menggunakan kode verifikasi: $code.";
+        $message     = "Halo, untuk mengaktifkan akun yang anda sedang dibuat, gunakan kode verifikasi: <br><h2><tt>$code</tt></h2><br>(<b>jangan sebarkan kesiapapun</b>).";
+        $message_alt = "Halo, untuk mengaktifkan akun yang anda sedang dibuat, gunakan kode verifikasi: \n$code\n(jangan sebarkan kesiapapun).";
     }
     else {
         $message_alt = strip_tags($message);
@@ -44,7 +44,7 @@ function sendVerification($to, $code, $message = ""){
         $mail->addAddress($to, 'Pengguna Cerdasly');
         $mail->addReplyTo($email, $sender);
         $mail->IsHTML(true);
-        $mail->Subject = "Selamat datang di Cerdasly - Ayo verifikasikan akunmu";
+        $mail->Subject = "Informasi penting dari Cerdasly";
         $mail->Body    = $message;
         $mail->AltBody = $message_alt;
         $mail->send();
