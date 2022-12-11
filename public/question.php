79855374bbf7852ee6ab1420be9da41723711c7f
azharrudin <azharrudin0905@gmail.com>
2022-12-11 19:41:59 +0700

    repository update

--- public/question.php
+++ public/question.php
@@ -456,6 +456,89 @@ if($is_question_exist == false) http_response_code(404);
      data-ad-slot="7867008779"
      data-ad-format="auto"
      data-full-width-responsive="true"></ins>
+
+
+     <footer class="text-center text-lg-start bg-light text-muted" >
+  <!-- Section: Links  -->
+  <section class="">
+    <div class="container text-center text-md-start mt-5">
+      <!-- Grid row -->
+      <div class="row mt-3"  id="ui_footer">
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <!-- Content -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            <i class="fas fa-gem me-3">Cerdasly</i>
+          </h6>
+          <p>
+          Tempat Digital untuk Berbagi dan Belajar Pengetahuan Baru :)
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Produk
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">Cerdasly Learn (coming soon)</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Cerdasly Forum</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Cerdasly Fact</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Bantuan
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">Ketentuan</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Pengguna</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Laporan</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">FAQ</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">Hubungi</h6>
+          <p><i class="fas fa-home me-3"></i> Jakarta Pusat, Petamburan 1 10260, Indonesia</p>
+          <p>
+            <i class="fas fa-envelope me-3"></i>
+            contact@cerdasly.com
+          </p>
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
+    </div>
+  </section>
+  <!-- Section: Links  -->
+
+  <!-- Copyright -->
+  <div class="text-center text-white p-4" style="background-color: rgba(220,53,69,0.8);">
+    <?= date("Y") ?> Copyright -
+    <a class="text-reset fw-bold" href="https://cerdasly.com/">Cerdasly App</a>
+  </div>
+  <!-- Copyright -->
+</footer>
 <script>
      (adsbygoogle = window.adsbygoogle || []).push({});
 </script>
